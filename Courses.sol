40f8fad0391e00bdadb4c7ce27255f67e27444b8
msharma <msharma@paypal.com>
2021-03-13 10:31:52 -0800

    Adding Inheritance

--- Courses.sol
+++ Courses.sol
@@ -2,7 +2,24 @@
 
 pragma solidity ^0.4.18;
 
-contract Courses {
+contract Owned {
+    address _owner;
+    
+    constructor() public {
+        _owner = msg.sender;
+    }
+    
+    function getContractOwner() view public returns (address) {
+        return _owner;
+    }
+    
+    modifier onlyOwner {
+        require (msg.sender == _owner);
+        _;
+    }
+}
+
+contract Courses is Owned {
     struct Instructor {
         uint256 age;
         string fName;
@@ -10,27 +27,28 @@ contract Courses {
     }
     
     mapping (address => Instructor) creator_instructor_map;
-    address[] creators;
+    address[] keys;
     
-    function addInstructor(address _creator, uint256 _age, string _fname, string _lname) public {
-        var instructor = creator_instructor_map[_creator];
+    // Adding the onlyOwner modifier ensures that only the owner that creates the contract instance can add instructors
+    function addInstructor(address _key, uint256 _age, string _fname, string _lname) onlyOwner public {
+        var instructor = creator_instructor_map[_key];
         
         instructor.age = _age;
         instructor.fName = _fname;
         instructor.lName = _lname;
         
-        creators.push(_creator) -1;
+        keys.push(_key) -1;
     }
     
-    function getCreators() view public returns(address[]) {
-        return creators;
+    function getKeys() view public returns(address[]) {
+        return keys;
     }
     
-    function getInstructor(address _creator) view public returns (uint256, string, string) {
-        return (creator_instructor_map[_creator].age, creator_instructor_map[_creator].fName, creator_instructor_map[_creator].lName);
+    function getInstructor(address _key) view public returns (uint256, string, string) {
+        return (creator_instructor_map[_key].age, creator_instructor_map[_key].fName, creator_instructor_map[_key].lName);
     }
     
-    function getCreatorsCount() view public returns (uint256) {
-        return creators.length;
+    function getKeysCount() view public returns (uint256) {
+        return keys.length;
     }
 }
